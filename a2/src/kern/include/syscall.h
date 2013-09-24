0a9f5b0475b2f8fab0ecbdb507adf6dce8dead74
Gabriel Luong <gabriel.luong@gmail.com>
2013-09-24 15:15:59 -0400

    Importing a2 starter code

---
+++ a2/src/kern/include/syscall.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2000, 2001, 2002, 2003, 2004, 2005, 2008, 2009
+ *	The President and Fellows of Harvard College.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE UNIVERSITY OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _SYSCALL_H_
+#define _SYSCALL_H_
+
+
+struct trapframe; /* from <machine/trapframe.h> */
+
+/*
+ * The system call dispatcher.
+ */
+
+void syscall(struct trapframe *tf);
+
+/*
+ * Support functions.
+ */
+
+/* Helper for fork(). You write this. */
+/* ASST2 - modified original signature to match the function that a 
+ * newly forked thread starts execution in. The first arg, data1, should
+ * be a pointer to a trapframe, copied from the parent.
+ */
+void enter_forked_process(void *data1, unsigned long unused);
+
+/* Enter user mode. Does not return. */
+void enter_new_process(int argc, userptr_t argv, vaddr_t stackptr,
+		       vaddr_t entrypoint);
+
+
+/*
+ * Prototypes for IN-KERNEL entry points for system call implementations.
+ */
+
+int sys_reboot(int code);
+int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
+
+/* ASST2 setup */
+int sys_fork(struct trapframe *tf, pid_t *retval);
+int sys_read(int fd, userptr_t buf, size_t size, int *retval);
+int sys_write(int fd, userptr_t buf, size_t size, int *retval);
+
+/*
+ * ASST2 - Prototypes for new bootstrap/shutdown functions needed by syscalls
+ */
+void dumb_consoleIO_bootstrap(void);
+
+#endif /* _SYSCALL_H_ */
